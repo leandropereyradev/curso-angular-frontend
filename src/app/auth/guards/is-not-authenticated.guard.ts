166bdeafbc46c9bf83374e337f934a2406d82adf
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-10-02 14:48:26 +0200

    isNotAuthenticated: Functional Guard

--- src/app/auth/guards/is-not-authenticated.guard.ts
+++ src/app/auth/guards/is-not-authenticated.guard.ts
@@ -3,14 +3,14 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { AuthStatus } from '../interfaces';
 
-export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.authStatus() === AuthStatus.checking) return false;
+  if (authService.authStatus() === AuthStatus.authtenticated) {
+    router.navigateByUrl('/dashboard');
+    return false;
+  }
 
-  if (authService.authStatus() === AuthStatus.authtenticated) return true;
-
-  router.navigateByUrl('/auth/login');
-  return false;
+  return true;
 };
